b25322ac29d040eeb62cd4c7e5ed77792f9f7b64
Pablo Tome Manzano <pablotomemanzano2000@gmail.com>
2026-09-23 21:54:28 +0200

    renaming DB

--- seeds/movies.seed.js
+++ seeds/movies.seed.js
@@ -41,7 +41,7 @@ const movies = [
 ];
 const movieDocuments = movies.map(movie => new Movie(movie));
 mongoose
-  .connect('mongodb://localhost:27017/proyecto-basico-express-movies')
+  .connect('mongodb://localhost:27017/proyecto5-noSQL-movies')
   .then(async () => {
     const allMovies = await Movie.find();
     if (allMovies.length) {
