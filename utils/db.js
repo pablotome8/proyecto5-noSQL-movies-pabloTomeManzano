ce221bb6113d4bfac13a68daa372585fd6f1f8ed
Pablo Tome Manzano <pablotomemanzano2000@gmail.com>
2026-09-23 21:45:16 +0200

    fix

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const urlDb = 'mongodb://localhost:27017/proyecto-basico-express-movies'
+const urlDb = 'mongodb://localhost:27017/proyecto5-noSQL-movies'
 
 const connect = async () => {
     try {
